c58fc0071c0576e32af36897fe9cf08956e21601
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-04-09 02:04:03 +0900

    Initial commit.

---
+++ HelloWorld/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloWorld.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message = "";
+        public string Answer = "";
+        public bool IsCorrect => Answer == "仙狐";
+
+        public void OnGet()
+        {
+            string answer = Request.Query["answer"];
+            Answer = answer;
+
+            if (IsCorrect)
+            {
+                Message = "正解";
+            }
+            else
+            {
+                Message = "不正解";
+            }
+        }
+    }
+}
